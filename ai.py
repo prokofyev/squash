b902900e374e6c648b6f68bf1104f3a471957864
Alexander Prokofyev <alexander-prokofyev@yandex.ru>
2026-04-26 22:51:29 +0300

    - В начальном меню можно выбрать, два игрока или один против компьютера

---
+++ ai.py
@@ -0,0 +1,20 @@
+import config
+
+class AIController:
+    def __init__(self, paddle):
+        self.paddle = paddle
+        self.deadzone = 10        # Зона покоя, чтобы не дёргался
+        self.speed_factor = 0.85  # Множитель скорости для баланса
+
+    def update(self, ball):
+        """Двигает ракетку ИИ, отслеживая мяч."""
+        # Реагируем только когда мяч летит вниз
+        if ball.dy < 0:
+            return
+
+        diff = ball.rect.centerx - self.paddle.rect.centerx
+        if abs(diff) > self.deadzone:
+            direction = 1 if diff > 0 else -1
+            self.paddle.rect.x += direction * config.PADDLE_SPEED * self.speed_factor
+        
+        self.paddle._clamp()
--- game.py
+++ game.py
@@ -1,13 +1,13 @@
+# game.py
 import pygame
 import config
 from paddle import Paddle
 from ball import Ball
+from ai import AIController
 
 class SquashGame:
     def __init__(self):
         pygame.init()
-        
-        # Настройка экрана: полный или оконный режим
         flags = pygame.FULLSCREEN if config.FULLSCREEN else 0
         if config.FULLSCREEN:
             info = pygame.display.Info()
@@ -21,37 +21,65 @@ class SquashGame:
         pygame.display.set_caption("Squash")
         self.clock = pygame.time.Clock()
         self.font = pygame.font.SysFont("arial", 44, bold=True)
-        self._setup()
+        self.sub_font = pygame.font.SysFont("arial", 28)
 
-    def _setup(self):
+        self.state = 'MENU'  # MENU, PLAYING, PAUSED, GAMEOVER
+        self.mode = '2P'     # '1P' или '2P'
+        self._setup_game()
+
+    def _setup_game(self):
         paddle_y = config.SCREEN_HEIGHT - config.PADDLE_Y_OFFSET
         self.p1 = Paddle(config.SCREEN_WIDTH // 4, paddle_y, config.P1_COLOR)
         self.p2 = Paddle(config.SCREEN_WIDTH * 3 // 4, paddle_y, config.P2_COLOR)
         self.ball = Ball()
+        self.ai = AIController(self.p2)
         self.score1 = 0
         self.score2 = 0
         self.next_hit = 1
-        self.is_running = True
         self.game_over = False
         self.winner_text = ""
         self.winner_color = config.WHITE
 
     def _handle_events(self):
-        keys = pygame.key.get_pressed()
-        self.p1.move(keys, config.P1_LEFT, config.P1_RIGHT)
-        self.p2.move(keys, config.P2_LEFT, config.P2_RIGHT)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.is_running = False
-            if event.type == pygame.KEYDOWN and event.key == config.RESTART_KEY and self.game_over:
-                self._setup()
-            if event.type == pygame.KEYDOWN and event.key == config.QUIT_KEY:
-                self.is_running = False
+                self.state = 'QUIT'
+                return
+
+            if event.type == pygame.KEYDOWN:
+                if self.state == 'MENU':
+                    if event.key == pygame.K_1:
+                        self.mode = '1P'
+                        self.state = 'PLAYING'
+                    elif event.key == pygame.K_2:
+                        self.mode = '2P'
+                        self.state = 'PLAYING'
+                    elif event.key == config.QUIT_KEY:  # <-- Выход по ESC из меню
+                        self.state = 'QUIT'
+                elif self.state == 'PLAYING':
+                    if event.key == config.QUIT_KEY:
+                        self.state = 'PAUSED'
+                elif self.state == 'PAUSED':
+                    if event.key == config.QUIT_KEY:
+                        self.state = 'PLAYING'
+                    elif event.key == config.RESTART_KEY:
+                        self.state = 'MENU'
+                elif self.state == 'GAMEOVER':
+                    if event.key == config.RESTART_KEY:
+                        self.state = 'MENU'
+
+        keys = pygame.key.get_pressed()
+        if self.state == 'PLAYING':
+            self.p1.move(keys, config.P1_LEFT, config.P1_RIGHT)
+            if self.mode == '2P':
+                self.p2.move(keys, config.P2_LEFT, config.P2_RIGHT)
 
     def _update(self):
-        if self.game_over:
+        if self.state != 'PLAYING':
             return
+
+        if self.mode == '1P':
+            self.ai.update(self.ball)
 
         self.ball.move()
         self.ball.bounce_walls()
@@ -59,18 +87,16 @@ class SquashGame:
         self._check_scoring()
 
     def _check_paddle_collision(self):
-        # Если мяч летит вверх, ракетки не обрабатываются
         if self.ball.dy < 0:
             return
-
         for paddle, player_id in [(self.p1, 1), (self.p2, 2)]:
             if self.ball.rect.colliderect(paddle.rect):
                 if self.next_hit == player_id:
                     self.ball.dy *= -1
                     self._apply_spin(paddle)
-                    self.next_hit = 3 - player_id  # Передаём очередь
-                    self.ball.rect.bottom = paddle.rect.top  # Корректировка позиции
-                    break  # Один контакт за кадр
+                    self.next_hit = 3 - player_id
+                    self.ball.rect.bottom = paddle.rect.top
+                    break
 
     def _apply_spin(self, paddle):
         hit_pos = (self.ball.rect.centerx - paddle.rect.centerx) / (paddle.rect.width / 2)
@@ -84,7 +110,6 @@ class SquashGame:
             else:
                 self.score1 += 1
                 self.next_hit = 1
-
             self._check_win()
             if not self.game_over:
                 self.ball.reset()
@@ -94,42 +119,76 @@ class SquashGame:
             self.game_over = True
             self.winner_text = "Синий победил!"
             self.winner_color = config.P1_COLOR
+            self.state = 'GAMEOVER'
         elif self.score2 >= config.WIN_SCORE:
             self.game_over = True
             self.winner_text = "Красный победил!"
             self.winner_color = config.P2_COLOR
+            self.state = 'GAMEOVER'
 
     def _draw(self):
         self.screen.fill(config.BLACK)
-        if self.next_hit == 2:
-            self.p1.draw(self.screen)
-            self.p2.draw(self.screen)
-        else:
-            self.p2.draw(self.screen)
-            self.p1.draw(self.screen)
-        self.ball.draw(self.screen)
-        self._draw_ui()
+
+        if self.state in ('PLAYING', 'PAUSED', 'GAMEOVER'):
+            # Активная ракетка рисуется сверху, чтобы не мешать прицеливанию
+            if self.next_hit == 1:
+                self.p2.draw(self.screen)
+                self.p1.draw(self.screen)
+            else:
+                self.p1.draw(self.screen)
+                self.p2.draw(self.screen)
+
+            self.ball.draw(self.screen)
+            self._draw_ui()
+
+        if self.state == 'MENU':
+            self._draw_menu()
+        elif self.state in ('PAUSED', 'GAMEOVER'):
+            self._draw_overlay()
+
         pygame.display.flip()
 
     def _draw_ui(self):
-        # Счёт по центру
         score_surf = self.font.render(f"{self.score1} : {self.score2}", True, config.WHITE)
         self.screen.blit(score_surf, (config.SCREEN_WIDTH // 2 - score_surf.get_width() // 2, 30))
 
-        # ЦВЕТНОЙ ИНДИКАТОР ХОДА (вместо текста)
         indicator_color = config.P1_COLOR if self.next_hit == 1 else config.P2_COLOR
         pygame.draw.rect(self.screen, indicator_color, (0, 0, config.SCREEN_WIDTH, config.PADDLE_HEIGHT))
 
-        if self.game_over:
+    def _draw_menu(self):
+        title = self.font.render("SQUASH", True, config.WHITE)
+        self.screen.blit(title, (config.SCREEN_WIDTH//2 - title.get_width()//2, 150))
+
+        opt1 = self.sub_font.render("[1] Один игрок (против компьютера)", True, config.GRAY)
+        opt2 = self.sub_font.render("[2] Два игрока", True, config.GRAY)
+        opt_esc = self.sub_font.render("[ESC] Выйти из игры", True, config.GRAY)
+
+        self.screen.blit(opt1, (config.SCREEN_WIDTH//2 - opt1.get_width()//2, 260))
+        self.screen.blit(opt2, (config.SCREEN_WIDTH//2 - opt2.get_width()//2, 310))
+        self.screen.blit(opt_esc, (config.SCREEN_WIDTH//2 - opt_esc.get_width()//2, 370))
+
+    def _draw_overlay(self):
+        surf = pygame.Surface((config.SCREEN_WIDTH, config.SCREEN_HEIGHT), pygame.SRCALPHA)
+        surf.fill((0, 0, 0, 140))
+        self.screen.blit(surf, (0, 0))
+
+        if self.state == 'GAMEOVER':
             win_surf = self.font.render(self.winner_text, True, self.winner_color)
-            self.screen.blit(win_surf, (config.SCREEN_WIDTH // 2 - win_surf.get_width() // 2, config.SCREEN_HEIGHT // 2 - 30))
-            
-            restart_font = pygame.font.SysFont("arial", 26)
-            restart_surf = restart_font.render("Нажмите пробел для новой игры или ESC для выхода", True, config.GRAY)
-            self.screen.blit(restart_surf, (config.SCREEN_WIDTH // 2 - restart_surf.get_width() // 2, config.SCREEN_HEIGHT // 2 + 20))
+            self.screen.blit(win_surf, (config.SCREEN_WIDTH//2 - win_surf.get_width()//2, config.SCREEN_HEIGHT//2 - 60))
+
+            txt = self.sub_font.render("ПРОБЕЛ - в главное меню", True, config.GRAY)
+            self.screen.blit(txt, (config.SCREEN_WIDTH//2 - txt.get_width()//2, config.SCREEN_HEIGHT//2 + 10))
+        else:
+            txt = self.font.render("ПАУЗА", True, config.WHITE)
+            self.screen.blit(txt, (config.SCREEN_WIDTH//2 - txt.get_width()//2, config.SCREEN_HEIGHT//2 - 40))
+
+            r1 = self.sub_font.render("ESC - продолжить", True, config.GRAY)
+            r2 = self.sub_font.render("ПРОБЕЛ - в главное меню", True, config.GRAY)
+            self.screen.blit(r1, (config.SCREEN_WIDTH//2 - r1.get_width()//2, config.SCREEN_HEIGHT//2 + 20))
+            self.screen.blit(r2, (config.SCREEN_WIDTH//2 - r2.get_width()//2, config.SCREEN_HEIGHT//2 + 60))
 
     def run(self):
-        while self.is_running:
+        while self.state != 'QUIT':
             self._handle_events()
             self._update()
             self._draw()
